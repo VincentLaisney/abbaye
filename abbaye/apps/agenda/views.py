fa03c4046042212f7e1a2d4de755f1ef32b1a598
BrRoman <editeur@traditions-monastiques.com>
2022-09-01 21:56:13 +0200

    Agenda: CRUD OK.

--- abbaye/apps/agenda/views.py
+++ abbaye/apps/agenda/views.py
@@ -1,7 +1,7 @@
 """ apps/agenda/views.py """
 
 from django.http import HttpResponseRedirect
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 from django.urls import reverse
 
 from apps.main.decorators import group_required
@@ -35,15 +35,57 @@ def create(request):
 
 
 def details(request, **kwargs):
-    """ Details of an agenda. """
-    pass
+    """ Details of an event. """
+    event = get_object_or_404(Event, id=kwargs['pk'])
+
+    return render(
+        request,
+        'agenda/details.html',
+        {
+            'event': event,
+        }
+    )
 
 
+@group_required('Agenda')
 def update(request, **kwargs):
-    """ Update an agenda. """
-    pass
+    """ Update an event. """
+    event = get_object_or_404(Event, pk=kwargs['pk'])
+
+    if request.method == 'POST':
+        form = EventForm(request.POST, instance=event)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect(
+                reverse(
+                    'agenda:details',
+                    kwargs={
+                        'pk': event.pk,
+                    }
+                )
+            )
+
+    form = EventForm(instance=event)
+
+    return render(request, 'agenda/form.html', {
+        'form': form,
+        'event': event,
+    })
 
 
+@group_required('Agenda')
 def delete(request, **kwargs):
-    """ Delete an agenda. """
-    pass
+    """ Delete an event. """
+    event = get_object_or_404(Event, pk=kwargs['pk'])
+
+    if request.method == 'POST':
+        form = EventForm(request.POST, instance=event)
+        event.delete()
+        return HttpResponseRedirect(reverse('agenda:home'))
+
+    form = EventForm(instance=event)
+
+    return render(request, 'agenda/delete.html', {
+        'form': form,
+        'event': event,
+    })
