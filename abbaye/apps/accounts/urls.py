6b15c6321e7c922bfabd0a52426e601a099674e6
BrRoman <editeur@traditions-monastiques.com>
2022-09-01 21:29:38 +0200

    Accounts: urls.py: slashes added.

--- abbaye/apps/accounts/urls.py
+++ abbaye/apps/accounts/urls.py
@@ -7,6 +7,6 @@ from . import views
 
 app_name = 'accounts'
 urlpatterns = [
-    path('login', views.AbbayeLoginView.as_view(), name='login'),
-    path('logout', LogoutView.as_view(), name='logout'),
+    path('login/', views.AbbayeLoginView.as_view(), name='login'),
+    path('logout/', LogoutView.as_view(), name='logout'),
 ]
