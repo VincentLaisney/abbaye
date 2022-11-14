60284b657967cf1809ffff8ec8da94e81451b1f6
BrRoman <editeur@traditions-monastiques.com>
2022-11-14 19:13:50 +0100

    Javascript: code formatting.

--- abbaye/apps/ornitho/static/ornitho/js/pictures.js
+++ abbaye/apps/ornitho/static/ornitho/js/pictures.js
@@ -1,20 +1,20 @@
 var links = document.querySelectorAll("td a");
 var links_length = links.length;
 
-for(var i = 0; i < links_length; i++){
-    links[i].addEventListener("click", function(e){
+for (var i = 0; i < links_length; i++) {
+    links[i].addEventListener("click", function (e) {
         e.preventDefault();
         displayImg(e.currentTarget, 900, 600);
-        });
+    });
 }
 
-function displayImg(link, maxW, maxH){
+function displayImg(link, maxW, maxH) {
     var img = new Image();
     var overlay = document.getElementById("overlay");
 
-    img.addEventListener("load", function(){
+    img.addEventListener("load", function () {
         overlay.innerHTML = "";
-        if(img.width > maxW || img.height > maxH)
+        if (img.width > maxW || img.height > maxH)
             var reduc = Math.max(img.width / maxW, img.height / maxH);
         else
             var reduc = 1;
@@ -22,15 +22,14 @@ function displayImg(link, maxW, maxH){
         img.height = Math.round(img.height / reduc);
         overlay.appendChild(img);
     });
-    
+
     img.src = link;
     overlay.style.height = window.innerHeight + "px";
     overlay.innerHTML = "<span>Chargement de l'image…</span>";
     overlay.style.display = "flex";
 }
 
-document.getElementById("overlay").addEventListener("click", function(e){
+document.getElementById("overlay").addEventListener("click", function (e) {
     e.currentTarget.innerHTML = "";
     e.currentTarget.style.display = "none";
 });
-
