e415f0a68b593f76103b1a1e1d4e90a627e87400
BrRoman <editeur@traditions-monastiques.com>
2023-02-14 04:31:51 +0100

    Admin: corrected url of the link to the site.

--- abbaye/abbaye/admin.py
+++ abbaye/abbaye/admin.py
@@ -5,4 +5,4 @@ from django.contrib import admin
 
 class MyAdminSite(admin.AdminSite):
     """ Overwrite admin.AdminSite. """
-    site_url = 'http://localhost:8080/abbaye/'
+    site_url = 'http://localhost:8080/'
