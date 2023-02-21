83a20538f9122882904527e8cf02c5735b3b7a71
BrRoman <editeur@traditions-monastiques.com>
2023-02-21 04:03:34 +0100

    Monks: tempusdominus-monks.js improved (array).

--- abbaye/apps/moines/static/moines/js/tempusdominus-moines.js
+++ abbaye/apps/moines/static/moines/js/tempusdominus-moines.js
@@ -1,7 +1,13 @@
-new tempusDominus.TempusDominus(document.getElementById('id_birthday'), calendar);
-new tempusDominus.TempusDominus(document.getElementById('id_entry'), calendar);
-new tempusDominus.TempusDominus(document.getElementById('id_habit'), calendar);
-new tempusDominus.TempusDominus(document.getElementById('id_profession_temp'), calendar);
-new tempusDominus.TempusDominus(document.getElementById('id_profession_perp'), calendar);
-new tempusDominus.TempusDominus(document.getElementById('id_priest'), calendar);
-new tempusDominus.TempusDominus(document.getElementById('id_death'), calendar);
+const ids = [
+    'id_birthday',
+    'id_entry',
+    'id_habit',
+    'id_profession_temp',
+    'id_profession_perp',
+    'id_priest',
+    'id_death',
+]
+
+ids.forEach(function (id) {
+    new tempusDominus.TempusDominus(document.getElementById(id), calendar);
+});
