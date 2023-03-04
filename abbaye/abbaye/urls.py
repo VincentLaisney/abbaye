7092221325fa4144b8aad8112e1ab0c14e80c598
BrRoman <editeur@traditions-monastiques.com>
2023-03-04 07:12:17 +0100

    Django-summernote removed.

--- abbaye/abbaye/urls.py
+++ abbaye/abbaye/urls.py
@@ -7,7 +7,6 @@ urlpatterns = [
     path('', include('apps.main.urls')),
     path('admin/', admin.site.urls),
     path('accounts/', include('apps.accounts.urls')),
-    path('summernote/', include('django_summernote.urls')),
 
     path('absences/', include('apps.absences.urls')),
     path('accenteur/', include('apps.accenteur.urls')),
