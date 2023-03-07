c93ab1c283b6a04414a47136ec1972488b46ae6f
BrRoman <editeur@traditions-monastiques.com>
2023-03-07 12:37:48 +0100

    Module dates improved (months, docstring).

--- abbaye/modules/dates.py
+++ abbaye/modules/dates.py
@@ -1,12 +1,27 @@
-""" Module dates """
+""" modules/dates.py """
+
+
+MONTHS = [
+    (1, 'janvier'),
+    (2, 'février'),
+    (3, 'mars'),
+    (4, 'avril'),
+    (5, 'mai'),
+    (6, 'juin'),
+    (7, 'juillet'),
+    (8, 'août'),
+    (9, 'septembre'),
+    (10, 'octobre'),
+    (11, 'novembre'),
+    (12, 'décembre'),
+]
 
 
 def date_to_french_string(date):
     """ Converts a date into french format. """
     weekdays = ['Lundi', 'Mardi', 'Mercredi',
                 'Jeudi', 'Vendredi', 'Samedi', 'Dimanche']
-    months = ['janvier', 'février', 'mars', 'avril', 'mai', 'juin',
-              'juillet', 'août', 'septembre', 'octobre', 'novembre', 'décembre']
+    months = [item[1] for item in MONTHS]
     weekday = weekdays[date.weekday()]
     day = date.day
     month = months[date.month - 1]
