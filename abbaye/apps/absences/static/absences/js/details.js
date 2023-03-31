4078e33a69274675ee88a4024c7fcedb089f1473
BrRoman <editeur@traditions-monastiques.com>
2023-03-31 21:38:49 +0200

    Absences: details.js.

---
+++ abbaye/apps/absences/static/absences/js/details.js
@@ -0,0 +1,17 @@
+// absences/details.js
+
+$(document).ready(function () {
+    // Date formatting:
+    $('.details_date').each(function () {
+        // Uppercase first letter:
+        const weekdays = ['dimanche', 'lundi', 'mardi', 'mercredi', 'jeudi', 'vendredi', 'samedi'];
+        for (const weekday of weekdays) {
+            $(this).html($(this).html().replace(
+                weekday,
+                first_letter_uppercase(weekday)
+            ));
+        }
+        // 1 => 1er.
+        $(this).html($(this).html().replace(" 1 ", " 1er "));
+    });
+});
