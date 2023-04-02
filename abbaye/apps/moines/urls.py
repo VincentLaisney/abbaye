ea6fa928fba37c93d9f833f383eb91289e9dbed2
BrRoman <editeur@traditions-monastiques.com>
2023-04-02 09:20:31 +0200

    Monks: home page removed.

--- abbaye/apps/moines/urls.py
+++ abbaye/apps/moines/urls.py
@@ -6,8 +6,7 @@ from . import views
 
 app_name = 'moines'
 urlpatterns = [
-    path('', views.home, name='home'),
-    path('list/', views.list, name='list'),
+    path('', views.list, name='list'),
     path('create/', views.create, name='create'),
     path('<int:pk>/', views.details, name='details'),
     path('<int:pk>/update/', views.update, name='update'),
