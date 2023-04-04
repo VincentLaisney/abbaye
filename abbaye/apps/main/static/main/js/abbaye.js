1d96ab08caec4231e083ee794693dc6e2689d487
BrRoman <editeur@traditions-monastiques.com>
2023-04-04 21:13:53 +0200

    Abbaye.js: date french formatting.

--- abbaye/apps/main/static/main/js/abbaye.js
+++ abbaye/apps/main/static/main/js/abbaye.js
@@ -10,3 +10,24 @@ $(document).ready(function () {
 function first_letter_uppercase(string) {
     return string.substring(0, 1).toUpperCase() + string.substring(1)
 }
+
+function string_to_date_object(string) {
+    // Return a date object from a 'dd/mm/yyyy' string.
+    return new Date(string.replace(/(\d{2})\/(\d{2})\/(\d{4})/, "$2/$1/$3"))
+}
+
+function date_to_french_string(date) {
+    // Return a french-formatted date from a date object.
+    return (
+        first_letter_uppercase(
+            new Intl.DateTimeFormat(
+                "fr-FR",
+                {
+                    dateStyle: 'full',
+                }
+            )
+                .format(date)
+        )
+            .replace(' 1 ', ' 1er ')
+    );
+}
