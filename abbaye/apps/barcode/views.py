5e13f11d06f16c83bf7ef088a467aff1215a8c41
BrRoman <editeur@traditions-monastiques.com>
2024-01-06 09:46:37 +0100

    Barcode put into this site.

--- abbaye/apps/barcode/views.py
+++ abbaye/apps/barcode/views.py
@@ -1,12 +1,36 @@
 """ apps/barcode/views.py """
 
+import os
+
+from django.http import JsonResponse
 from django.shortcuts import render
 
 
 def home(request):
-    """ Home page of Barcode. """
+    """ Home page. """
     return render(
         request,
         'barcode/home.html',
         {},
+    )
+
+
+def create(request, **kwargs):
+    """ Create the image of the barcode (if needed). """
+    code = kwargs['barcode']
+    path = "/home/frromain/Sites/abbaye/abbaye/statics/barcode/img/archives/{0}.png".format(
+        code)
+
+    if not os.path.exists(path):
+        os.system(
+            "barcode -b {0} -e 'ean13' -u mm -g 100x50 -S -o /home/frromain/Sites/abbaye/abbaye/apps/barcode/static/barcode/img/barcode.svg; \
+            convert /home/frromain/Sites/abbaye/abbaye/apps/barcode/static/barcode/img/barcode.svg -transparent '#FFFFFF' -trim /home/frromain/Sites/abbaye/abbaye/apps/barcode/static/barcode/img/archives/{0}.png; \
+            rm /home/frromain/Sites/abbaye/abbaye/apps/barcode/static/barcode/img/*.svg; \
+            cp /home/frromain/Sites/abbaye/abbaye/apps/barcode/static/barcode/img/archives/{0}.png /home/frromain/Sites/abbaye/abbaye/statics/barcode/img/archives/{0}.png"
+            .format(code))
+
+    return JsonResponse(
+        {
+            'status': 'ready',
+        },
     )
