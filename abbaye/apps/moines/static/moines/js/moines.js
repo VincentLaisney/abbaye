bf91e488293e5aaac9248df718481382b672b10d
BrRoman <editeur@traditions-monastiques.com>
2024-05-13 15:54:29 +0200

    Monks: tempusdominus => jquery-ui datepicker.

--- abbaye/apps/moines/static/moines/js/moines.js
+++ abbaye/apps/moines/static/moines/js/moines.js
@@ -1,7 +1,26 @@
 $(document).ready(function () {
-    $('label:contains("<i>")').each(function () {
-        $(this).html(
-            $(this).html().replace(/&lt;i&gt;(.*)&lt;\/i&gt;/g, "<i>$1</i > ")
-        );
-    });
+  // Lier le datepicker à l'input date :
+  $(function () {
+    var values = {
+      dateFormat: "dd/mm/yy",
+      minDate: null,
+      dayNames: ["Dimanche", "Lundi", "Mardi", "Mercredi", "Jeudi", "Vendredi", "Samedi"],
+      dayNamesMin: ["Di", "Lu", "Ma", "Me", "Je", "Ve", "Sa"],
+      monthNames: ["Janvier", "Février", "Mars", "Avril", "Mai", "Juin", "Juillet", "Août", "Septembre", "Octobre", "Novembre", "Décembre"],
+      monthNamesShort: ["Jan", "Fév", "Mar", "Avr", "Mai", "Juin", "Juil", "Aoû", "Sep", "Oct", "Nov", "Déc"]
+    }
+    $("#id_birthday").datepicker(values);
+    $("#id_entry").datepicker(values);
+    $("#id_habit").datepicker(values);
+    $("#id_profession_temp").datepicker(values);
+    $("#id_profession_perp").datepicker(values);
+    $("#id_priest").datepicker(values);
+    $("#id_death").datepicker(values);
+  });
+
+  $('label:contains("<i>")').each(function () {
+    $(this).html(
+      $(this).html().replace(/&lt;i&gt;(.*)&lt;\/i&gt;/g, "<i>$1</i > ")
+    );
+  });
 });
