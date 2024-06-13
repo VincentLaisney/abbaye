d36bbc93531d7b6db7e27704efec8f1149ff7df6
BrRoman <editeur@traditions-monastiques.com>
2024-06-13 10:31:17 +0200

    Agenda: urls corr. + formatting.

--- abbaye/apps/agenda/urls.py
+++ abbaye/apps/agenda/urls.py
@@ -7,9 +7,17 @@ from . import views
 app_name = 'agenda'
 urlpatterns = [
     path('list/', views.agenda_as_list, name='list'),
-    path('list/<str:date>/', views.agenda_as_list, name='list_with_date'),
+    path(
+        'list/<str:date>/',
+        views.agenda_as_list,
+        name='list_with_date'
+    ),
     path('calendar/', views.agenda_as_calendar, name='calendar'),
-    path('calendar/<str:date>/', views.agenda_as_calendar, name='calendar'),
+    path(
+        'calendar/<str:date>/',
+        views.agenda_as_calendar,
+        name='calendar_with_date'
+    ),
     path('create/', views.create, name='create'),
     path('<int:pk>/', views.details, name='details'),
     path('<int:pk>/update/', views.update, name='update'),
