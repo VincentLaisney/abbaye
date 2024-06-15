bccd75ce16a1f91a402a0d38f9dc2889994c2ad3
BrRoman <editeur@traditions-monastiques.com>
2024-06-15 05:09:57 +0200

    Agenda: urls: details: slash added.

--- abbaye/apps/absences/urls.py
+++ abbaye/apps/absences/urls.py
@@ -8,7 +8,7 @@ app_name = 'absences'
 urlpatterns = [
     path('', views.list, name='list'),
     path('create/', views.create, name='create'),
-    path('<pk>', views.details, name='details'),
+    path('<pk>/', views.details, name='details'),
     path('<pk>/update/', views.update, name='update'),
     path('<pk>/delete/', views.delete, name='delete'),
 ]
