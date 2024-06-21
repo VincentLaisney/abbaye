0f17ce062ba1ae4a2c94b0eeb56a35bb21eb8b85
BrRoman <editeur@traditions-monastiques.com>
2024-06-21 16:47:26 +0200

    Ordomatic: write tex: first steps.

--- abbaye/apps/ordomatic/views.py
+++ abbaye/apps/ordomatic/views.py
@@ -1,5 +1,7 @@
 """ apps/ordomatic/views.py """
 
+import os
+from pathlib import Path
 from datetime import date
 
 from django.http import JsonResponse
@@ -21,6 +23,7 @@ def home(request):
 def pdf(request, *args, **kwargs):
     """ Create the PDF of the ordo. """
     year = kwargs['year']
+    write_pdf(year)
     return (
         JsonResponse(
             {
@@ -28,3 +31,15 @@ def pdf(request, *args, **kwargs):
             },
         )
     )
+
+
+def write_pdf(year):
+    """ Write the PDF of the Ordo according to the year. """
+    tex = ''
+    tex = '\\input{config.tex}\\par\n\n'
+    tex += '\\begin{document}\\par\n'
+    tex += 'Ordo {} !\\par\n'.format(year)
+    tex += '\\end{document}\n'
+
+    with open(os.path.join(Path(__file__).resolve().parent, 'tex/ordo.tex'), 'w') as tex_file:
+        tex_file.write(tex)
