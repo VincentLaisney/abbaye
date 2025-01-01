465d1e95a596c909c6bc7fa327697ab08765b708
BrRoman <editeur@traditions-monastiques.com>
2025-01-01 08:46:11 +0100

    Livrets: date.year => get_*liturgical*_year().

--- abbaye/apps/livrets/views.py
+++ abbaye/apps/livrets/views.py
@@ -10,7 +10,7 @@ from django.conf import settings
 from django.http import JsonResponse
 from django.shortcuts import render
 
-from modules.calendar import get_liturgical_day
+from modules.calendar import get_liturgical_day, get_liturgical_year
 
 from .models import BMV, Day, Preface, Score
 
@@ -96,8 +96,9 @@ def pdf(request):
     tex += "\\TitreA{Messe conventuelle}\n"
     for i in range(number_of_days):
         date = start + datetime.timedelta(days=i)
-        year_cycle = ['A', 'B', 'C'][(date.year - 2020) % 3]
-        year_even = 2 if date.year % 2 == 0 else 1
+        liturgical_year = get_liturgical_year(date)
+        year_cycle = ['A', 'B', 'C'][(liturgical_year - 2020) % 3]
+        year_even = 2 if liturgical_year % 2 == 0 else 1
         data_tempo, data_sancto, liturgical_day = get_data(date)
         data = data_tempo
         data['tempo'] = data['ref']
