e8bfe4a0e00d1cc975eb6bad1873bd9c5206e02b
BrRoman <editeur@traditions-monastiques.com>
2025-01-01 08:41:41 +0100

    Admin.py: site_url: port 8000.

--- abbaye/abbaye/admin.py
+++ abbaye/abbaye/admin.py
@@ -5,4 +5,4 @@ from django.contrib import admin
 
 class MyAdminSite(admin.AdminSite):
     """ Overwrite admin.AdminSite. """
-    site_url = 'http://localhost:8080/'
+    site_url = 'http://localhost:8000/'
