5d3d0a3625b79e320a4af18c105e3468f1496de7
Vincent Laisney <vincent.laisney@biblico.it>
2025-12-04 11:20:42 +0100

    Barcode: new paths

--- abbaye/apps/barcode/views.py
+++ abbaye/apps/barcode/views.py
@@ -20,10 +20,10 @@ def create_barcode(request, **kwargs):
     code = kwargs['barcode']
 
     os.system(
-        "barcode -b {0} -e 'ean13' -u mm -g 100x50 -S -o /home/frromain/Sites/abbaye/abbaye/apps/barcode/static/barcode/img/barcode.svg; \
-        convert /home/frromain/Sites/abbaye/abbaye/apps/barcode/static/barcode/img/barcode.svg -transparent '#FFFFFF' -trim /home/frromain/Sites/abbaye/abbaye/apps/barcode/static/barcode/img/barcode.png; \
-        rm /home/frromain/Sites/abbaye/abbaye/apps/barcode/static/barcode/img/*.svg; \
-        cp /home/frromain/Sites/abbaye/abbaye/apps/barcode/static/barcode/img/barcode.png /home/frromain/Sites/abbaye/abbaye/media/barcode/barcode.png"
+        "barcode -b {0} -e 'ean13' -u mm -g 100x50 -S -o /home/vincent/Sites/abbaye/abbaye/apps/barcode/static/barcode/img/barcode.svg; \
+        convert /home/vincent/Sites/abbaye/abbaye/apps/barcode/static/barcode/img/barcode.svg -transparent '#FFFFFF' -trim /home/vincent/Sites/abbaye/abbaye/apps/barcode/static/barcode/img/barcode.png; \
+        rm /home/vincent/Sites/abbaye/abbaye/apps/barcode/static/barcode/img/*.svg; \
+        cp /home/vincent/Sites/abbaye/abbaye/apps/barcode/static/barcode/img/barcode.png /home/vincent/Sites/abbaye/abbaye/media/barcode/barcode.png"
         .format(code))
 
     return JsonResponse(
@@ -38,8 +38,8 @@ def create_qrcode(request, **kwargs):
     code = kwargs['qrcode']
 
     os.system(
-        "qrencode -o /home/frromain/Sites/abbaye/abbaye/apps/barcode/static/barcode/img/qrcode.png {0}; \
-        cp /home/frromain/Sites/abbaye/abbaye/apps/barcode/static/barcode/img/qrcode.png /home/frromain/Sites/abbaye/abbaye/media/barcode/qrcode.png"
+        "qrencode -o /home/vincent/Sites/abbaye/abbaye/apps/barcode/static/barcode/img/qrcode.png {0}; \
+        cp /home/vincent/Sites/abbaye/abbaye/apps/barcode/static/barcode/img/qrcode.png /home/vincent/Sites/abbaye/abbaye/media/barcode/qrcode.png"
         .format(code))
 
     return JsonResponse(
