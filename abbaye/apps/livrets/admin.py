fd3aecfdd133aa85439781b7dc75314abbc7e5d5
Vincent Laisney <vincent.laisney@biblico.it>
2025-12-19 16:53:28 +0100

    Livrets: Better display of Day and Score databases

--- abbaye/apps/livrets/admin.py
+++ abbaye/apps/livrets/admin.py
@@ -3,7 +3,19 @@
 from django.contrib import admin
 from .models import Day, BMV, Preface, Score
 
-admin.site.register(Day)
+#admin.site.register(Day)
+@admin.register(Day)
+class DayAdmin(admin.ModelAdmin):
+    list_display = ['ref', 'title', 'rang', 'precedence']
+    list_filter = ['precedence']
+    search_fields = ['ref']
+
 admin.site.register(BMV)
 admin.site.register(Preface)
-admin.site.register(Score)
+
+@admin.register(Score)
+class ScoreAdmin(admin.ModelAdmin):
+    list_display = ['type', 'ref', 'name', 'page']
+    list_filter = ['type']
+    search_fields = ['ref']
+
