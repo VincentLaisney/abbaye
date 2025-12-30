b800db795e0ed7788c0233738044bcf98fb0f3d1
Vincent Laisney <vincent.laisney@biblico.it>
2025-12-30 16:11:39 +0100

    Hotellerie: Add databases in Admin

--- abbaye/apps/hotellerie/admin.py
+++ abbaye/apps/hotellerie/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Personne, Chambre, Sejour, Parloir, Mail, Telephone, Adresse, Retreat
 
-# Register your models here.
+admin.site.register(Personne)
+admin.site.register(Chambre)
+admin.site.register(Sejour)
+admin.site.register(Parloir)
+admin.site.register(Mail)
+admin.site.register(Telephone)
+admin.site.register(Adresse)
+admin.site.register(Retreat)
